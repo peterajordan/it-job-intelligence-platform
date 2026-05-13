582b2b79f1864f5d0afa1015960f2603bb8d3442
Peter Jordan <pjgoldnight@gmail.com>
2026-05-13 08:57:13 -0400

    Create opportunity_scoring_agent.js

---
+++ agents/opportunity_scoring_agent.js
@@ -0,0 +1,12 @@
+/*
+Filtering and Classification Agent
+
+Responsible for:
+- Keyword filtering
+- Remote opportunity detection
+- Duplicate removal
+*/
+
+function filterJobs() {
+   console.log("Filtering job opportunities...");
+}
